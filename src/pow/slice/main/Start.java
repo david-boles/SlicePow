5e169df6d3011480058606a06fd71db1f3e59c40
david476 <david.e.boles22@gmail.com>
2016-02-11 10:27:03 -0800

    Added try statement to console controls

--- src/pow/slice/main/Start.java
+++ src/pow/slice/main/Start.java
@@ -30,49 +30,53 @@ public class Start {
 		Scanner scan = new Scanner(System.in);
 		while(!Thread.interrupted()) {
 			String in = scan.nextLine();
-			if(in.equals("stop")) {
-				scan.close();
-				stop();
-			}
-			else if(in.equals("reset")) {
-				ArrayList<SlicedURL> urls = URLManager.getAll();
-				synchronized (urls) {
-					urls.clear();
+			try {
+				if(in.equals("stop")) {
+					scan.close();
+					stop();
 				}
-			}
-			else if(in.equals("list")) {
-				ArrayList<SlicedURL> urls = URLManager.getAll();
-				synchronized (urls) {
-					for(int i = 0; i < urls.size(); i++) {
-						SlicedURL sUrl = urls.get(i);
-						Logger.uLogger.logMore("short URL, URL, hours remaining", new Object[]{sUrl.shortenedURL, sUrl.url, (sUrl.decompDelay-(System.currentTimeMillis()-sUrl.creationTime))/(float)(1000*60*60)});
+				else if(in.equals("reset")) {
+					ArrayList<SlicedURL> urls = URLManager.getAll();
+					synchronized (urls) {
+						urls.clear();
 					}
 				}
-			}else if(in.equals("listinf")) {
-				ArrayList<SlicedURL> urls = URLManager.getAll();
-				synchronized (urls) {
-					for(int i = 0; i < urls.size(); i++) {
-						SlicedURL sUrl = urls.get(i);
-						if(sUrl.decompDelay == Long.MAX_VALUE) Logger.uLogger.logMore("short URL, URL, hours remaining", new Object[]{sUrl.shortenedURL, sUrl.url, (sUrl.decompDelay-(System.currentTimeMillis()-sUrl.creationTime))/(float)(1000*60*60)});
+				else if(in.equals("list")) {
+					ArrayList<SlicedURL> urls = URLManager.getAll();
+					synchronized (urls) {
+						for(int i = 0; i < urls.size(); i++) {
+							SlicedURL sUrl = urls.get(i);
+							Logger.uLogger.logMore("short URL, URL, hours remaining", new Object[]{sUrl.shortenedURL, sUrl.url, (sUrl.decompDelay-(System.currentTimeMillis()-sUrl.creationTime))/(float)(1000*60*60)});
+						}
 					}
+				}else if(in.equals("listinf")) {
+					ArrayList<SlicedURL> urls = URLManager.getAll();
+					synchronized (urls) {
+						for(int i = 0; i < urls.size(); i++) {
+							SlicedURL sUrl = urls.get(i);
+							if(sUrl.decompDelay == Long.MAX_VALUE) Logger.uLogger.logMore("short URL, URL, hours remaining", new Object[]{sUrl.shortenedURL, sUrl.url, (sUrl.decompDelay-(System.currentTimeMillis()-sUrl.creationTime))/(float)(1000*60*60)});
+						}
+					}
+				}else if(in.equals("save")) {
+					URLManager.saveURLs();
+				}else if(in.equals("load")) {
+					URLManager.loadURLs();
 				}
-			}else if(in.equals("save")) {
-				URLManager.saveURLs();
-			}else if(in.equals("load")) {
-				URLManager.loadURLs();
-			}
-			else if(in.startsWith("remove")) {
-				System.out.println(in);
-				String url = in.substring(7);
-				System.out.println(url);
-				Logger.uLogger.log("Successful?", URLManager.removeURL(url));
-			}
-			else if(in.startsWith("sremove")) {
-				String url = in.substring(8);
-				Logger.uLogger.log("Successful?", URLManager.removeSlicedURL(url));
-			}
-			else {
-				Logger.uLogger.log("Incorrect command.");
+				else if(in.startsWith("remove")) {
+					System.out.println(in);
+					String url = in.substring(7);
+					System.out.println(url);
+					Logger.uLogger.log("Successful?", URLManager.removeURL(url));
+				}
+				else if(in.startsWith("sremove")) {
+					String url = in.substring(8);
+					Logger.uLogger.log("Successful?", URLManager.removeSlicedURL(url));
+				}
+				else {
+					Logger.uLogger.log("Incorrect command.");
+				}
+			}catch(Exception e) {
+				Logger.uLogger.exception("Controls", e);
 			}
 		}
 	}
